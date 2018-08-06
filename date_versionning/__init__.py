b3e78db53d4a18ba8c815d1c5586a59035e8264f
Rémi Lapeyre <remi.lapeyre@henki.fr>
2018-08-06 18:10:48 +0200

    Initial commit

---
+++ date_versionning/__init__.py
@@ -0,0 +1,165 @@
+import copy
+
+from datetime import datetime
+from rest_framework.versioning import BaseVersioning
+from rest_framework import exceptions, serializers
+from django.utils.translation import ugettext_lazy as _
+from rest_framework.fields import empty
+
+
+class DateHeaderVersioning(BaseVersioning):
+    invalid_version_message = _('Invalid version in "X-Version" header.')
+
+    def determine_version(self, request, *args, **kwargs):
+        try:
+            version = request.META['X-Version']
+        except KeyError:
+            return datetime.now().strftime(r'%Y-%m-%d')
+
+        try:
+            datetime.strptime(version, r'%Y-%m-%d')
+            return version
+        except ValueError:
+            raise exceptions.NotAcceptable(self.invalid_version_message)
+
+
+class APIChange:
+    """This is a no-op API change"""
+
+    def update(self, payload):
+        return payload
+
+    def downgrade(self, fields=None, payload=None):
+        return payload
+
+
+class RemoveField(APIChange):
+    def __init__(self, name, field, default=None):
+        self.name = name
+        self.default = default
+        self.field = field
+
+    def get_value(self, payload):
+        return self.default
+
+    def update(self, payload=None):
+        if payload is not None:
+            del payload[self.name]
+        return payload
+
+    def downgrade(self, fields=None, payload=None):
+        if fields is not None:
+            fields[self.name] = copy.deepcopy(self.field)
+        if payload is not None:
+            payload[self.name] = self.get_value(payload)
+        return fields, payload
+
+
+class RenameField:
+    def __init__(self, from_name, to):
+        self.from_name = from_name
+        self.to = to
+
+    def downgrade(self, fields=None, payload=None):
+        if fields is not None:
+            fields[self.from_name] = copy.deepcopy(fields[self.to])
+            del fields[self.to]
+        if payload is not None:
+            payload[self.from_name] = payload[self.to]
+            del payload[self.to]
+        return fields, payload
+
+    def update(self, payload=None):
+        if payload is not None:
+            payload[self.to] = payload[self.from_name]
+            del payload[self.from_name]
+        return payload
+
+
+class AddField:
+    def __init__(self, name, field, default=None):
+        self.name = name
+        self.field = field
+        self.default = default
+
+    def get_value(self, payload):
+        return self.default
+
+    def downgrade(self, fields=None, payload=None):
+        if fields is not None:
+            del fields[self.name]
+        if payload is not None:
+            del payload[self.name]
+        return fields, payload
+
+    def update(self, payload=None):
+        if payload is not None:
+            payload[self.name] = self.get_value(payload)
+        return payload
+
+
+class VersionnedSerializer(serializers.Serializer):
+    @property
+    def versions(self):
+        if self.version is None:
+            return {}
+
+        meta = getattr(self, 'Meta', {})
+        versions = getattr(meta, 'versions', {})
+
+        return {
+            name: v for name, v in versions.items()
+            if name >= self.version
+        }
+
+    @property
+    def version(self):
+        try:
+            request = self._context['request']
+            return request.version
+        except (KeyError, AttributeError):
+            return None
+
+    @property
+    def updated_data(self):
+        data = copy.deepcopy(self.data)
+
+        for v in reversed(list(self.versions.values())):
+            data = v.update(payload=data)
+
+        return data
+
+
+    @property
+    def data(self):
+        if hasattr(self, 'initial_data') and not hasattr(self, '_validated_data'):
+            msg = (
+                'When a serializer is passed a `data` keyword argument you '
+                'must call `.is_valid()` before attempting to access the '
+                'serialized `.data` representation.\n'
+                'You should either call `.is_valid()` first, '
+                'or access `.initial_data` instead.'
+            )
+            raise AssertionError(msg)
+
+        if not hasattr(self, '_data'):
+            if self.instance is not None and not getattr(self, '_errors', None):
+                # We instanciate a serializer with the same fields but at the latest
+                # version to serialize the instance and the downgrade the result
+                data = self.__class__().to_representation(self.instance)
+                for v in self.versions.values():
+                    data = v.downgrade(payload=data)[1]
+                self._data = data
+            elif hasattr(self, '_validated_data') and not getattr(self, '_errors', None):
+                self._data = self.to_representation(self.validated_data)
+            else:
+                self._data = self.get_initial()
+        return self._data
+
+    def get_fields(self):
+        fields = super(VersionnedSerializer, self).get_fields()
+
+        for v in self.versions.values():
+            fields = v.downgrade(fields=fields)[0]
+
+        return fields
---
+++ date_versionning/test_versionning.py
@@ -0,0 +1,421 @@
+import pytest
+import rest_framework
+
+from datetime import datetime
+from collections import OrderedDict
+from textwrap import dedent
+from django.db import models
+from rest_framework import serializers
+from rest_framework.decorators import APIView
+from rest_framework.response import Response
+from . import (DateHeaderVersioning, APIChange, RemoveField, RenameField, AddField,
+               VersionnedSerializer)
+
+
+class SimpleSerializer(serializers.Serializer):
+    firstname = serializers.CharField()
+    lastname = serializers.CharField()
+
+
+class TestDetermineVersion:
+    def test_no_versionning(self, rf):
+        request = rf.get('/')
+        assert DateHeaderVersioning().determine_version(request) == datetime.now().strftime(r'%Y-%m-%d')
+
+    def test_invalid_version(self, rf):
+        request = rf.get('/', **{'X-Version': 'this is invalid'})
+        with pytest.raises(rest_framework.exceptions.NotAcceptable):
+            DateHeaderVersioning().determine_version(request)
+
+    def test_valid_version(self, rf):
+        request = rf.get('/', **{'X-Version': '2018-08-03'})
+        assert DateHeaderVersioning().determine_version(request) == '2018-08-03'
+
+
+class TestOperations:
+    chewby = {
+      "name": "Chewbacca",
+      "birthYear": "200BBY",
+      "eyeColor": "blue",
+      "gender": "male",
+      "hairColor": "brown",
+      "height": 228,
+      "mass": 112,
+      "homeworld": {
+        "name": "Kashyyyk"
+      }
+    }
+
+    def test_no_op(self):
+        change = APIChange()
+        previous_payload = change.downgrade(self.chewby)
+        assert previous_payload == self.chewby
+        assert change.update(previous_payload) == self.chewby
+
+    def test_remove_field(self):
+        change = RemoveField('hairStyle', serializers.CharField())
+        previous_payload = change.downgrade(payload=self.chewby)[1]
+        assert previous_payload == {
+            "name": "Chewbacca",
+            "birthYear": "200BBY",
+            "eyeColor": "blue",
+            "gender": "male",
+            "hairColor": "brown",
+            "hairStyle": None,
+            "height": 228,
+            "mass": 112,
+            "homeworld": {
+                "name": "Kashyyyk"
+            }
+        }
+
+        previous_payload['hairStyle'] = 'fluffy'
+        assert change.update(payload=previous_payload) == self.chewby
+
+        serializer = SimpleSerializer()
+        fields = change.downgrade(fields=serializer._declared_fields)[0]
+        assert list(fields) == ['firstname', 'lastname', 'hairStyle']
+
+    def test_rename_field(self):
+        change = RenameField('origin', 'homeworld')
+        previous_payload = change.downgrade(payload=self.chewby)[1]
+        assert 'homeworld' not in previous_payload
+        assert previous_payload == {
+            "name": "Chewbacca",
+            "birthYear": "200BBY",
+            "eyeColor": "blue",
+            "gender": "male",
+            "hairColor": "brown",
+            "height": 228,
+            "mass": 112,
+            "origin": {
+                "name": "Kashyyyk"
+            }
+        }
+
+        assert change.update(payload=previous_payload) == self.chewby
+
+    def test_add_field(self):
+        change = AddField('hairStyle', serializers.CharField())
+        next_payload = change.update(payload=self.chewby)
+        assert next_payload == {
+            "name": "Chewbacca",
+            "birthYear": "200BBY",
+            "eyeColor": "blue",
+            "gender": "male",
+            "hairColor": "brown",
+            "hairStyle": None,
+            "height": 228,
+            "mass": 112,
+            "homeworld": {
+                "name": "Kashyyyk"
+            }
+        }
+
+        assert change.downgrade(payload=next_payload)[1] == self.chewby
+
+
+instance = {
+        "name": "Chewbacca",
+        "birthYear": "200BBY",
+        "eyeColor": "blue",
+        "gender": "male",
+        "hairColor": "brown",
+        "height": 228,
+        "mass": 112,
+        "homeworld": {
+            "name": "Kashyyyk"
+        }
+    }
+
+class HomeworldSerializer(VersionnedSerializer):
+    name = serializers.CharField()
+
+
+class PersonSerializer(VersionnedSerializer):
+    name = serializers.CharField()
+    birthYear = serializers.CharField()
+    eyeColor = serializers.CharField()
+    gender = serializers.CharField()
+    hairColor = serializers.CharField()
+    height = serializers.IntegerField()
+    mass = serializers.IntegerField()
+    homeworld = HomeworldSerializer()
+
+    class Meta:
+        versions = {
+            '2018-08-02': RemoveField('hairStyle', serializers.CharField()),
+            '2018-07-29': RenameField('iColor', 'eyeColor'),
+            '2018-07-27': AddField('gender', serializers.CharField(), default='male')
+        }
+
+
+class VersionView(APIView):
+    versioning_class = DateHeaderVersioning
+
+    def get(self, request, *args, **kwargs):
+        return Response({'version': request.version})
+
+
+class TestVersionnedSerializer:
+    def test_without_context(self):
+        assert repr(PersonSerializer()) == dedent("""\
+        PersonSerializer():
+            name = CharField()
+            birthYear = CharField()
+            eyeColor = CharField()
+            gender = CharField()
+            hairColor = CharField()
+            height = IntegerField()
+            mass = IntegerField()
+            homeworld = HomeworldSerializer():
+                name = CharField()""")
+
+    def test_without_version(self, rf):
+        request = rf.get('/')
+        assert repr(PersonSerializer(context={'request': request})) == dedent("""\
+        PersonSerializer(context={'request': <WSGIRequest: GET '/'>}):
+            name = CharField()
+            birthYear = CharField()
+            eyeColor = CharField()
+            gender = CharField()
+            hairColor = CharField()
+            height = IntegerField()
+            mass = IntegerField()
+            homeworld = HomeworldSerializer():
+                name = CharField()""")
+
+        serializer_instance = PersonSerializer(instance=instance)
+        serializer_data = PersonSerializer(data=instance)
+        serializer_data.is_valid(raise_exception=True)
+        assert serializer_instance.data == serializer_data.validated_data
+
+    def test_with_version(self, rf):
+        request = rf.get('/')
+        request.version = '2018-08-01'
+        assert repr(PersonSerializer(context={'request': request})) == dedent("""\
+        PersonSerializer(context={'request': <WSGIRequest: GET '/'>}):
+            name = CharField()
+            birthYear = CharField()
+            eyeColor = CharField()
+            gender = CharField()
+            hairColor = CharField()
+            height = IntegerField()
+            mass = IntegerField()
+            homeworld = HomeworldSerializer():
+                name = CharField()
+            hairStyle = CharField()""")
+
+    def test_with_version_and_data(self, rf):
+        request = rf.get('/')
+        request.version = '2018-08-01'
+        data = {
+            "name": "Chewbacca",
+            "birthYear": "200BBY",
+            "eyeColor": "blue",
+            "gender": "male",
+            "hairColor": "brown",
+            "hairStyle": "fluffy",
+            "height": 228,
+            "mass": 112,
+            "homeworld": {
+                "name": "Kashyyyk"
+            }
+        }
+        serializer_data = PersonSerializer(
+            data=data,
+            context={'request': request}
+        )
+        serializer_data.is_valid(raise_exception=True)
+        assert serializer_data.data['hairStyle'] == 'fluffy'
+        assert serializer_data.updated_data == instance
+        assert serializer_data.data != instance
+
+        serializer_instance = PersonSerializer(
+            instance=instance,
+            context={'request': request}
+        )
+        assert serializer_instance.data['hairStyle'] == None
+
+    def test_rename_field(self, rf):
+        request = rf.get('/')
+        request.version = '2018-07-29'
+        serializer = PersonSerializer(
+            data={
+                "name": "Chewbacca",
+                "birthYear": "200BBY",
+                "iColor": "blue",
+                "gender": "male",
+                "hairColor": "brown",
+                "hairStyle": "fluffy",
+                "height": 228,
+                "mass": 112,
+                "homeworld": {
+                    "name": "Kashyyyk"
+                }
+            },
+            context={'request': request}
+        )
+        serializer.is_valid(raise_exception=True)
+        assert serializer.data == {
+            "name": "Chewbacca",
+            "birthYear": "200BBY",
+            "iColor": "blue",
+            "gender": "male",
+            "hairColor": "brown",
+            "hairStyle": "fluffy",
+            "height": 228,
+            "mass": 112,
+            "homeworld": {
+                "name": "Kashyyyk"
+            }
+        }
+        assert serializer.updated_data == {
+            "name": "Chewbacca",
+            "birthYear": "200BBY",
+            "eyeColor": "blue",
+            "gender": "male",
+            "hairColor": "brown",
+            "height": 228,
+            "mass": 112,
+            "homeworld": {
+                "name": "Kashyyyk"
+            }
+        }
+
+    def test_add_field(self, rf):
+        request = rf.get('/')
+        request.version = '2018-07-26'
+        serializer = PersonSerializer(
+            data={
+                "name": "Chewbacca",
+                "birthYear": "200BBY",
+                "iColor": "blue",
+                "hairColor": "brown",
+                "hairStyle": "fluffy",
+                "height": 228,
+                "mass": 112,
+                "homeworld": {
+                    "name": "Kashyyyk"
+                }
+            },
+            context={'request': request}
+        )
+        serializer.is_valid(raise_exception=True)
+        assert serializer.data == {
+            "name": "Chewbacca",
+            "birthYear": "200BBY",
+            "iColor": "blue",
+            "hairColor": "brown",
+            "hairStyle": "fluffy",
+            "height": 228,
+            "mass": 112,
+            "homeworld": {
+                "name": "Kashyyyk"
+            }
+        }
+        assert serializer.updated_data == {
+            "name": "Chewbacca",
+            "birthYear": "200BBY",
+            "eyeColor": "blue",
+            "gender": "male",
+            "hairColor": "brown",
+            "height": 228,
+            "mass": 112,
+            "homeworld": {
+                "name": "Kashyyyk"
+            }
+        }
+
+
+class Homeworld(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=20)
+    birthYear = models.CharField(max_length=4)
+    eyeColor = models.CharField(max_length=10)
+    gender = models.CharField(max_length=6)
+    hairColor = models.CharField(max_length=12)
+    height = models.IntegerField()
+    mass = models.IntegerField()
+    homeworld = models.ForeignKey(Homeworld, on_delete=models.CASCADE)
+
+
+class TestInstance:
+    @pytest.mark.django_db
+    def test_without_context(self):
+        kashyyyk = Homeworld(name='Kashyyyk')
+        chewby = Person(
+            name="Chewbacca",
+            birthYear="200BBY",
+            eyeColor="blue",
+            gender="male",
+            hairColor="brown",
+            height=228,
+            mass=112,
+            homeworld=kashyyyk
+        )
+        serializer = PersonSerializer(instance=chewby)
+        assert serializer.data == {
+            'name': 'Chewbacca',
+            'birthYear': '200BBY',
+            'eyeColor': 'blue',
+            'gender': 'male',
+            'hairColor': 'brown',
+            'height': 228,
+            'mass': 112,
+            'homeworld': {
+                'name': 'Kashyyyk'
+            }
+        }
+
+        serializer = PersonSerializer(
+            instance=chewby,
+            data={
+                "name": "Chewbacca",
+                "birthYear": "200BBY",
+                "eyeColor": "blue",
+                "gender": "male",
+                "hairColor": "brown",
+                "height": 228,
+                "mass": 112,
+                "homeworld": {
+                    "name": "Kashyyyk"
+                }
+            }
+        )
+        assert serializer.is_valid(raise_exception=True)
+
+    # def test_with_context(self, rf):
+    #     request = rf.get('/')
+    #     request.version = '2018-07-26'
+
+    #     kashyyyk = Homeworld(name='Kashyyyk')
+    #     chewby = Person(
+    #         name="Chewbacca",
+    #         birthYear="200BBY",
+    #         eyeColor="blue",
+    #         gender="male",
+    #         hairColor="brown",
+    #         height=228,
+    #         mass=112,
+    #         homeworld=kashyyyk
+    #     )
+    #     serializer = PersonSerializer(
+    #         instance=chewby,
+    #         context={'request': request}
+    #     )
+    #     serializer.data == {
+    #         'name': 'Chewbacca',
+    #         'birthYear': '200BBY',
+    #         'eyeColor': 'blue',
+    #         'gender': 'male',
+    #         'hairColor': 'brown',
+    #         'height': 228,
+    #         'mass': 112,
+    #         'homeworld': {
+    #             'name': 'Kashyyyk'
+    #         }
+    #     }
